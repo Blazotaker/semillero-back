20d006f6e581f464b082621fb6bd4bd0218f5a70
Alejandro Cortés <josealejandro221@gmail.com>
2019-11-18 21:00:19 -0500

    Almoste done

--- app/Http/Controllers/ActividadController.php
+++ app/Http/Controllers/ActividadController.php
@@ -18,8 +18,8 @@ class ActividadController extends Controller
     {
         try {
             $actividad = DB::table('actividades')
-            ->join('periodos', 'periodos.id_periodo', 'actividades.id_periodo')
-            ->join('semilleros', 'semilleros.id_semillero', 'periodos.id_semillero')
+                ->join('periodos', 'periodos.id_periodo', 'actividades.id_periodo')
+                ->join('semilleros', 'semilleros.id_semillero', 'periodos.id_semillero')
                 ->get();
             if ($actividad->isEmpty()) {
                 return response()->json('', 204);
@@ -88,8 +88,8 @@ class ActividadController extends Controller
                 $datos['meses'] = [];
                 $m1 = [];
                 $i = 0;
-                foreach($mes_actividad as $mes){
-                    array_push($datos['meses'],(int)$mes['id_mes']);
+                foreach ($mes_actividad as $mes) {
+                    array_push($datos['meses'], (int) $mes['id_mes']);
                 }
                 return $datos;
             }
@@ -100,33 +100,48 @@ class ActividadController extends Controller
     public function actividadesPeriodoSemillero($id_periodo)
     {
         try {
-            $actividades = Actividad::select(
+            $actividades = collect(Actividad::select(
                 'actividades.id_actividad',
                 'actividad',
+                'meses.id_mes',
                 'actividades.responsable',
                 'actividades.recursos',
                 'actividades.registro',
                 'actividades.estado'
-            )
-                ->leftJoin('periodos', 'periodos.id_periodo', 'actividades.id_periodo')
+            )->leftJoin('periodos', 'periodos.id_periodo', 'actividades.id_periodo')
                 ->leftJoin('semilleros', 'semilleros.id_semillero', 'periodos.id_semillero')
                 ->leftJoin('mes_actividades', 'mes_actividades.id_actividad', 'actividades.id_actividad')
                 ->leftJoin('meses', 'meses.id_mes', 'mes_actividades.id_mes')
-                ->where('actividades.id_periodo', $id_periodo)->first();
-
-            if ($actividades == null ) {
+                ->where('actividades.id_periodo', $id_periodo)
+                ->get());
+                $actividadesUnico = $actividades->unique('id_actividad');
+                //return $actividadesUnico;
+            if ($actividades->isEmpty()) {
                 return response()->json('', 204);
             } else {
-                $datos = [];
-                $datos['actividad'] = $actividades;
-                $mes_actividad = Mes_actividad::select('id_mes')->where('id_actividad', $id_periodo)->get();
-                $datos['meses'] = [];
-                $m1 = [];
-                $i = 0;
-                foreach($mes_actividad as $mes){
-                    array_push($datos['meses'],(int)$mes['id_mes']);
+                $contenido = [];
+                $datos = json_decode($actividadesUnico, true);
+
+                //return $datos;
+                $union = [];
+                //return $datos;
+
+                foreach ($datos as $dato) {
+                    $i = 0;
+                    $dato['meses'] = [];
+                    $mes_actividad = Mes_actividad::select('id_mes')->where('id_actividad', $dato['id_actividad'])->get();
+                    // return $actividad['id_actividad'];
+                    $meses['meses'] = [];
+                    foreach ($mes_actividad as $mes) {
+                        array_push($meses['meses'], $mes['id_mes']);
+                    }
+                    //return $meses;
+                    array_push($dato['meses'],$meses);
+                    //return $dato;
+                    array_push($contenido, $dato);
+                    $i++;
                 }
-                return $datos;
+                return $contenido;
                 return $actividades;
             }
         } catch (\Exception $e) {
@@ -156,8 +171,8 @@ class ActividadController extends Controller
                 $datos['meses'] = [];
                 $m1 = [];
                 $i = 0;
-                foreach($mes_actividad as $mes){
-                    array_push($datos['meses'],(int)$mes['id_mes']);
+                foreach ($mes_actividad as $mes) {
+                    array_push($datos['meses'], (int) $mes['id_mes']);
                 }
                 return $datos;
             }
@@ -183,7 +198,7 @@ class ActividadController extends Controller
             } else {
                 Actividad::where('id_actividad', $id)
                     ->update($request->all());
-                    return response()->json("Actividad actualizada");
+                return response()->json("Actividad actualizada");
             }
         } catch (\Exception $e) {
             return response()->json($e->getMessage(), 400);
